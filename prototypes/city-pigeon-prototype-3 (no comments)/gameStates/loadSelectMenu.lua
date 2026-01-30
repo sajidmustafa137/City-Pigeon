108c6a086acc3ad7f4d483c3286c41f97986c24a
sajidmustafa137 <sajidmustafa137@gmail.com>
2026-01-30 03:32:44 -0500

    Add files via upload

---
+++ prototypes/city-pigeon-prototype-3 (no comments)/gameStates/loadSelectMenu.lua
@@ -0,0 +1,17 @@
+local function initialize(world)
+    
+end
+
+local function update()
+    
+end
+
+local function draw()
+    
+end
+
+return {
+    initialize = initialize,
+    update = update,
+    draw = draw,
+}
---
+++ prototypes/city-pigeon-prototype-3 (no comments)/levels/initializeTiles/finish.lua
@@ -0,0 +1,5 @@
+local function initializeFinish(body, shape, fixture)
+  fixture:setSensor(true)
+end
+
+return initializeFinish
